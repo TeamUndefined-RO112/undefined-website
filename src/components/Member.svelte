7f46e16f66aee09d18b07e7c7ae2bd2a7275b10c
ImperiaL-24 <53470229+ImperiaL-24@users.noreply.github.com>
2022-08-24 22:55:22 +0300

    member card styling

--- src/components/Member.svelte
+++ src/components/Member.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-import Content from "./Content.svelte";
 
     type Member = {
         src: string,
@@ -8,18 +7,32 @@ import Content from "./Content.svelte";
         desc: string
     }
     export let member: Member;
+
+    let colorOfDepartment= function (department:string) {
+        let sel = 0;
+        let arg = department.split("/");
+        if(arg.length != 1) sel = 1;
+        if(arg[sel][0]=="V") return "gray";
+        if(arg[sel]=="Hardware") return "green";
+        if(arg[sel]=="Programming") return "orange";
+        if(arg[sel]=="Engineering Notebook") return "red";
+        if(arg[sel]=="Marketing") return "purple";
+        if(arg[sel]=="Team Leader") return "blue";
+    }
 </script>
 
-<Content>
+<div class="wrapper {colorOfDepartment(member.department)}">
     <img src={member.src} alt={member.name}>
-    <div>
-        <p>{member.name}</p>
-        <p>{member.department}</p>
-        <p>{member.desc}</p>
+    <div class="content">
+        <div class="title-wrap">
+            <p class="title">{member.name}</p>
+            <p class="dep">{member.department}</p>
+        </div>
+        <p class="desc">{member.desc}</p>
     </div>
 
 
-</Content>
+</div>
 
 <style lang="scss">
 img {
@@ -34,8 +47,71 @@ p {
     margin: 0;
 }
 
-div {
+.wrapper {
+    box-shadow: 0px 4px 10px gray;
+    border-radius: 5px;
+    padding: 16px;
+    color: #ffffff;
+    max-width: 500px;
+
+
     display: flex;
-    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+    .content {
+        align-items: baseline;
+        justify-content: end;
+        gap: 0px;
+
+        .desc {
+            font-weight: 300;
+        }
+    }
+
+    .title-wrap {
+        justify-content: end;
+        gap: 0px;
+        margin-bottom: 10px;
+
+        .dep {
+            font-size: 0.8em;
+            color: rgba(255, 255, 255, 0.8);
+        }
+
+        .title {
+            font-size: 1.2em;
+        }
+
+
+    }
 }
+
+.green {
+  background-color: rgb(79, 165, 86);
+}
+
+.orange {
+  background-color: rgb(219, 115, 46);
+}
+
+.gray {
+  background-color: rgb(92, 92, 92);
+}
+
+.red {
+  background-color: rgb(194, 52, 52);
+}
+
+.black {
+  background-color: rgb(15, 15, 15);
+}
+
+.purple {
+  background-color: rgb(75, 40, 104);
+}
+
+.blue {
+  background-color: rgb(57, 45, 160);
+}
+
 </style>
