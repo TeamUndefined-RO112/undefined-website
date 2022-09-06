0ef3981e2b628a7c96c5aa50be3640c4f31ce185
ImperiaL-24 <53470229+ImperiaL-24@users.noreply.github.com>
2022-09-06 18:05:30 +0300

    sveltekit test2

--- _app/immutable/chunks/0-734166e4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-de25dea7.js";export{t as component};
---
+++ _app/immutable/chunks/1-51cc367e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-c7d2f59a.js";export{t as component};
