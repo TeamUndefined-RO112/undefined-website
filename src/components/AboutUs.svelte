8ef7165ee1e7ce6deca4ba9c0dade9b8d1a7a5e5
Andrei Stahie <stahie.andrei.m@gmail.com>
2024-09-16 12:07:12 +0300

    Descrierea modificărilor tale

--- src/components/AboutUs.svelte
+++ src/components/AboutUs.svelte
@@ -4,16 +4,16 @@
     import Member from "./Member.svelte";
     import Title from "./Title.svelte";
     import members from './members.json';
-    let year: number = 2021;
+    let year: number = 2024;
 </script>
 
 <Content column big>
     <Title>Our Team</Title>
     <Title secondary>Year</Title>
     <div class="flex justify-center items-center gap-2">
-        <Button small on:click={() => {if(year>2018) year--; if(year==2019) year--;}}>-</Button>
+        <Button small on:click={() => {if(year>2018) year--; if(year==2019) year--;if(year==2022) year--;}}>-</Button>
         <p>{year}</p>
-        <Button small on:click={() => {if(year<2022) year++; if(year==2019) year++;}}>+</Button>
+        <Button small on:click={() => {if(year<2024) year++; if(year==2019) year++;if(year==2022) year++;}}>+</Button>
     </div>
 </Content>
 
