bfcd49dd5063913cc1692cce7879c3d020781bba
Traian <53470229+ImperiaL-24@users.noreply.github.com>
2024-09-17 13:47:28 +0300

    Update copyright

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,27 @@
-<footer class="w-full bg-gray-200 h-36 flex flex-col items-center gap-2" >
-    <br>
-    <hr class="w-11/12 border-gray-500 mt-4">
+<footer class="w-full bg-gray-200 h-36 flex flex-col items-center gap-2">
+    <br />
+    <hr class="w-11/12 border-gray-500 mt-4" />
     <div class="w-11/12 text-gray-600">
-        <p class="inline text-xs">© 2022 Team Undefined</p>
+        <p class="inline text-xs">© 2024 Team Undefined</p>
         <ul class="float-right m-0 flex items-center">
-            <li class="p-2 pt-0"><a href="https://www.instagram.com/team_undefined/" target="_blank">Instagram</a></li>
-            <li class="p-2 pt-0"><a href="https://www.facebook.com/teamundefinedcnng/" target="_blank">Facebook</a></li>
-            <li class="p-2 pt-0"><a href="https://www.youtube.com/channel/UCN4OeYg_hz3U1jyXbdKV9cw" target="_blank">Youtube</a></li>
+            <li class="p-2 pt-0">
+                <a
+                    href="https://www.instagram.com/team_undefined/"
+                    target="_blank">Instagram</a
+                >
+            </li>
+            <li class="p-2 pt-0">
+                <a
+                    href="https://www.facebook.com/teamundefinedcnng/"
+                    target="_blank">Facebook</a
+                >
+            </li>
+            <li class="p-2 pt-0">
+                <a
+                    href="https://www.youtube.com/channel/UCN4OeYg_hz3U1jyXbdKV9cw"
+                    target="_blank">Youtube</a
+                >
+            </li>
         </ul>
     </div>
 </footer>
